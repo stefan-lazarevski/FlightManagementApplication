acddb31230c8061c3a7ba2c82e76a2665724906f
stefan-lazarevski <lazarevski055@gmail.com>
2025-03-12 23:57:16 +0100

    changes to models.py and admin.py

--- FlightApp/admin.py
+++ FlightApp/admin.py
@@ -1,3 +1,42 @@
 from django.contrib import admin
+from .models import Pilot, Airline, AirLinePilot, Balloon, Flight
+
 
 # Register your models here.
+class AirLinePilotInline(admin.TabularInline):
+    model = AirLinePilot
+    extra = 0
+
+class AirlineAdmin(admin.ModelAdmin):
+    list_display = ("name", "year_founded", "outside_of_Europe")
+    inlines = [AirLinePilotInline]
+
+    def has_add_permission(self, request):
+        return True
+
+class PilotAdmin(admin.ModelAdmin):
+    list_display = ("name", "lastname")
+
+    def has_delete_permission(self, request, obj=None):
+        if request.user.is_superuser:
+            return True
+        return False
+
+
+class FlightAdmin(admin.ModelAdmin):
+    exclude = ("user",)
+
+    def save_model(self, request, obj, form, change):
+        obj.user = request.user
+        return super(FlightAdmin, self).save_model(request, obj, form, change)
+
+    def has_change_permission(self, request, obj = None):
+        if obj and obj.user == request.user:
+            return True
+        return False
+
+
+admin.site.register(Flight, FlightAdmin)
+admin.site.register(Balloon)
+admin.site.register(Pilot, PilotAdmin)
+admin.site.register(Airline, AirlineAdmin)
--- FlightApp/models.py
+++ FlightApp/models.py
@@ -1,3 +1,63 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+class Airline(models.Model):
+    name = models.CharField(max_length=50)
+    year_founded = models.IntegerField()
+    outside_of_Europe = models.BooleanField()
+
+    def __str__(self):
+        return self.name
+
+class Balloon(models.Model):
+    type = [
+        ("S", "Small Balloon"),
+        ("M", "Medium Balloon"),
+        ("L", "Large Balloon"),
+    ]
+
+    name = models.CharField(max_length=50)
+    type = models.CharField(max_length=1, choices=type)
+    manufacturer = models.CharField(max_length=100)
+    max_passengers = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.name} ({self.get_type_display()})"
+
+class Pilot(models.Model):
+    rank = [
+        ("J", "Junior"),
+        ("I", "Intermediate"),
+        ("S", "Senior")
+    ]
+
+    name = models.CharField(max_length=50)
+    lastname = models.CharField(max_length=50)
+    year_of_birth = models.IntegerField()
+    total_flight_hours = models.IntegerField()
+    rank = models.CharField(max_length=1, choices=rank)
+
+    def __str__(self):
+        return f"{self.name} {self.lastname} ({self.get_rank_display()})"
+
+class AirLinePilot(models.Model):
+    airline = models.ForeignKey(Airline, on_delete=models.CASCADE)
+    pilot = models.ForeignKey(Pilot, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.airline} {self.pilot}"
+
+class Flight(models.Model):
+    code = models.CharField(max_length=10, unique=True)
+    take_off_airport = models.CharField(max_length=50)
+    landing_airport = models.CharField(max_length=50)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    photo = models.ImageField(upload_to='flight_photos/', null=True, blank=True)
+    date = models.DateField()
+    airline = models.ForeignKey(Airline, on_delete=models.CASCADE)
+    balloon = models.ForeignKey(Balloon, on_delete=models.CASCADE)
+    pilot = models.ForeignKey(Pilot, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Flight {self.code} ({self.take_off_airport} to {self.landing_airport})"
